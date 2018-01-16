7df537802c498519b2f20c9005666d57f610dbdf
NuwanthiYapa <35219619+NuwanthiYapa@users.noreply.github.com>
2018-01-16 10:37:49 +0530

    add DDB with put method

--- ProjectNu24_160118/ProjectNu24_160118-lambda.js
+++ ProjectNu24_160118/ProjectNu24_160118-lambda.js
@@ -1,6 +1,17 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 	console.log("Event triggered ", event);
+	ddb.put({
+		TableName: 'nuwanthi_table',
+		Item: { first_name: 'nuwanthi', last_name: 'yapa', id: 1 }
+	}, function (err, data) {
+		if (err) {
+			console.log("Error ", err);
+		} else {
+			console.log("Data ", data);
+		}
+	});
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 }
