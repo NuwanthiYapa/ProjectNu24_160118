7550800f863a9ab67399aea24f18c31252a9a599
NuwanthiYapa <35219619+NuwanthiYapa@users.noreply.github.com>
2018-01-16 11:55:41 +0530

    add RDS

--- ProjectNu24_160118/ProjectNu24_160118-lambda.js
+++ ProjectNu24_160118/ProjectNu24_160118-lambda.js
@@ -1,7 +1,26 @@
 let AWS = require('aws-sdk');
+let connectionManager = require('./ConnectionManager');
+let SL = require('slappforge-aws');
+const rds = new SL.RDS(connectionManager);
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	console.log("Event triggered ", event);
+
+	// Replace the query with the actual query
+	rds.query({
+		identifier: 'nuwanthi',
+		query: '<query>',
+		transactional: true
+	}, function (error, results, fields) {
+		if (error) {
+			console.log("Error occurred");
+			throw error;
+		} else {
+			console.log("Success")
+			console.log(results);
+		}
+	});
+
 	ddb.put({
 		TableName: 'nuwanthi_table',
 		Item: {
@@ -17,7 +36,7 @@ exports.handler = function (event, context, callback) {
 		}
 	});
 
-	
+
 
 	callback(null, 'Successfully executed');
 }
